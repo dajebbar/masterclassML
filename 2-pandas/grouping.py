960212605054b6785cb472d4e4174ccb7881b64a
dajebbar <dajebbar@gmx.fr>
2022-09-08 12:51:53 +0100

    cross xs key level

--- 2-pandas/grouping.py
+++ 2-pandas/grouping.py
@@ -8,12 +8,20 @@ def main():
     # print(df.shape)
     # print(df['model_year'].unique())
     
+    # groupby
     # print(df.groupby(['model_year']).mean())
     # print(df.groupby(['model_year', 'cylinders']).mean()[['mpg']])
     year_cyl = df.groupby(['model_year', 'cylinders']).mean()
     # print(year_cyl)
     # print(year_cyl.loc[[70, 80]])
-    print(year_cyl.loc[(82, 4)])
+    # print(year_cyl.loc[(82, 4)])
+    
+    # Cross section
+    # print(year_cyl.xs(key=78, level='model_year'))
+    print(year_cyl.xs(key=4, level='cylinders'))
+    
+    
+    
     
 
 if __name__=='__main__':
