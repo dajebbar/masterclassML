8fcdf795ed5cac50457a9cd2046c6e6e0d35f533
dajebbar <dajebbar@gmx.fr>
2022-09-08 11:31:57 +0100

    isnull

---
+++ 2-pandas/misssing_vals.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+pd.set_option('display.max_columns', 0)
+
+# Missing values options:
+    # - Save
+    # -Delete
+    # - replace
+
+def main():
+    movies = pd.read_csv('nb/Datasets/movie_scores.csv')
+    # print(movies)
+    # print(movies.shape)
+    print(movies.isnull())
+    print(movies.isnull().sum())
+
+if __name__=='__main__':
+    main()
