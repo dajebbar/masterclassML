197c64c02dd1f50df055a8ea7aa421dba19c3b81
dajebbar <dajebbar@gmx.fr>
2022-09-08 13:30:32 +0100

    grouping

--- 2-pandas/grouping.py
+++ 2-pandas/grouping.py
@@ -4,31 +4,31 @@ import pandas as pd
 
 def main():
     df = pd.read_csv('nb/Datasets/mpg.csv')
-    # print(df.head())
-    # print(df.shape)
-    # print(df['model_year'].unique())
+    print(df.head())
+    print(df.shape)
+    print(df['model_year'].unique())
     
     # groupby
-    # print(df.groupby(['model_year']).mean())
-    # print(df.groupby(['model_year', 'cylinders']).mean()[['mpg']])
+    print(df.groupby(['model_year']).mean())
+    print(df.groupby(['model_year', 'cylinders']).mean()[['mpg']])
     year_cyl = df.groupby(['model_year', 'cylinders']).mean()
-    # print(year_cyl)
-    # print(year_cyl.loc[[70, 80]])
-    # print(year_cyl.loc[(82, 4)])
+    print(year_cyl)
+    print(year_cyl.loc[[70, 80]])
+    print(year_cyl.loc[(82, 4)])
     
     # Cross section
-    # print(year_cyl.xs(key=78, level='model_year'))
-    # print(year_cyl.xs(key=4, level='cylinders'))
+    print(year_cyl.xs(key=78, level='model_year'))
+    print(year_cyl.xs(key=4, level='cylinders'))
     
     # filtering before grouping
-    # print(df[df['cylinders'].isin([6, 8])].groupby(['model_year', 'cylinders']).mean())
+    print(df[df['cylinders'].isin([6, 8])].groupby(['model_year', 'cylinders']).mean())
     
     # sorting index
-    # print(year_cyl.sort_index(level='model_year', ascending=False))
+    print(year_cyl.sort_index(level='model_year', ascending=False))
     
     # .agg()
-    # print(year_cyl.agg(['mean', 'std']))
-    # print(year_cyl.agg(['mean', 'std'])['mpg'])
+    print(year_cyl.agg(['mean', 'std']))
+    print(year_cyl.agg(['mean', 'std'])['mpg'])
     print(df.agg({'mpg':['mean', 'max'], 'weight': ['std', 'mean']}))
     
     
