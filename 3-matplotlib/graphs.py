231b09610daec6e0722f4c7129cd3388e43191d5
dajebbar <dajebbar@gmx.fr>
2022-09-20 17:09:24 +0100

    sub

--- 3-matplotlib/graphs.py
+++ 3-matplotlib/graphs.py
@@ -5,12 +5,26 @@ import matplotlib.pyplot as plt
 
 x = np.arange(10)
 y = x **2 +3*x - 5
+a = np.linspace(0, 10, 11)
+b = a **4
+
 plt.figure(figsize=(6, 4))
+
+plt.subplot(1, 2, 1)
 plt.plot(x, y)
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.xlim(0, 10)
 plt.ylim(0, 110)
 plt.title('Quadratic')
+
+plt.subplot(1, 2, 2)
+plt.plot(a, b)
+plt.xlabel('a')
+plt.ylabel('b')
+plt.xlim(0, 10)
+plt.ylim(0, 11000)
+plt.title('X⁴')
+
 plt.show()
-plt.savefig('../graphs/plot.png')
+# plt.savefig('../graphs/plot.png')
