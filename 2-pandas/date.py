16e45ba256f9580c47659e6f987403b12184e0be
dajebbar <dajebbar@gmx.fr>
2022-09-20 13:38:03 +0100

    serie to_datetime

--- 2-pandas/date.py
+++ 2-pandas/date.py
@@ -9,6 +9,13 @@ min = 30
 sec = 18
 
 m_date = datetime.datetime(yr, mth, d)
-print(m_date)
+# print(m_date)
 m_date = datetime.datetime(yr, mth, d, hr, min, sec)
-print(m_date)
+# print(m_date)
+
+s = pd.Series(['25 Nov 2021', '10-04-1991', None])
+# print(s)
+s_date = pd.to_datetime(s)
+print()
+# print(s_date)
+print(s_date[0].month)
