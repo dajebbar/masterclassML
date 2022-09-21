03e32876525551c152ac55a483b107ac17a63794
dajebbar <dajebbar@gmx.fr>
2022-09-21 14:16:28 +0100

    add axes

--- 3-matplotlib/graphs.py
+++ 3-matplotlib/graphs.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+plt.style.use('ggplot')
 
 x = np.arange(10)
 y = x **2 +3*x - 5
 a = np.linspace(0, 10, 11)
 b = a **4
 
-plt.figure(figsize=(6, 4))
+# plt.figure(figsize=(6, 4))
 
-plt.subplot(1, 2, 1)
+'''plt.subplot(1, 2, 1)
 plt.plot(x, y)
 plt.xlabel('X')
 plt.ylabel('Y')
@@ -25,6 +25,10 @@ plt.ylabel('b')
 plt.xlim(0, 10)
 plt.ylim(0, 11000)
 plt.title('X⁴')
+'''
 
+fig = plt.figure()
+axes = fig.add_axes([0, 0, 1, 1])
+axes.plot(a, b)
 plt.show()
 # plt.savefig('../graphs/plot.png')
