e3ab690e308acc3865c915cd4666cb17a1e70d63
dajebbar <dajebbar@gmx.fr>
2022-09-22 15:28:19 +0100

    pandas cut

--- 2-pandas/num_to_cat.py
+++ 2-pandas/num_to_cat.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
-fp = 'bit.ly/felonies-dataset'
-df = pd.DataFrame(fp)
-print(df.head())
-print()
-print(df.tail())
-print()
-print(df.info())
+fp = 'https://bit.ly/felonies-dataset'
+df = pd.read_csv(fp)
+# print(df.head())
+# print()
+# print(df.tail())
+# print()
+# print(df.info())
+
+# print(df.Age.describe())
+
+df['age_bin'] = pd.cut(df['Age'], bins=[9, 13, 20, 65, 100], labels=['child', 'taneeger', 'adult', 'senior'])
+# print(df['age_bin'].head())
+print(df.sample(10))
+
