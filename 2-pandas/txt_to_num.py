7c41e97202db02772042d60aa3fbee257c393d33
dajebbar <dajebbar@gmx.fr>
2022-09-22 13:20:03 +0100

    btc wire_amount from txt to float

--- 2-pandas/txt_to_num.py
+++ 2-pandas/txt_to_num.py
@@ -6,4 +6,8 @@ fp = 'https://bit.ly/bitcoin-purchases'
 bitcoin = pd.read_csv(fp)
 
 bitcoin.columns = bitcoin.columns.str.lower().str.replace(' ', '_')
+
+bitcoin['wire_amount'] = bitcoin['wire_amount'].apply(lambda num: float(num.replace('$', '')))
 print(bitcoin.sample(10))
+print()
+print(bitcoin['wire_amount'].dtype)
