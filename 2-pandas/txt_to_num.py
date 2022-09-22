b3bc0be1490dc7aacf2e3c4987bcd1be6c7dd580
dajebbar <dajebbar@gmx.fr>
2022-09-22 13:15:52 +0100

    btc clean column index

--- 2-pandas/txt_to_num.py
+++ 2-pandas/txt_to_num.py
@@ -4,4 +4,6 @@ import numpy as np
 
 fp = 'https://bit.ly/bitcoin-purchases'
 bitcoin = pd.read_csv(fp)
+
+bitcoin.columns = bitcoin.columns.str.lower().str.replace(' ', '_')
 print(bitcoin.sample(10))
