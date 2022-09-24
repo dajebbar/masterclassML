e19b7020e1a922d13b020da21467ad365849e1df
dajebbar <dajebbar@gmx.fr>
2022-09-24 16:10:16 +0100

    recap

--- 2-pandas/m_vals2.py
+++ 2-pandas/m_vals2.py
@@ -4,10 +4,16 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 fp = 'https://bit.ly/missing-values'
+fp2 = 'https://bit.ly/missing-values-toy'
 
 df = pd.read_csv(fp)
 # print(df.tail())
 
 # print(df.isna().sum())
+# print(df.color.value_counts(dropna=False))
 
-print(df.color.value_counts(dropna=False))
+df2 = pd.read_csv(fp2)
+print(df2.head())
+print(df2.isna().sum())
+print(df2.isna().sum().sum())
+print(df2['Colonne Importante'].value_counts(dropna=False))
