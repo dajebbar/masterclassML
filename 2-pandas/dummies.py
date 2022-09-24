2f304d7831937664884abc5f5329ba213d003eac
dajebbar <dajebbar@gmx.fr>
2022-09-24 17:38:05 +0100

    ohe with dummies

--- 2-pandas/dummies.py
+++ 2-pandas/dummies.py
@@ -5,8 +5,9 @@ fp = 'https://bit.ly/dummy-variables'
 df = pd.read_csv(fp)
 c = df.color.value_counts(dropna=False)
 prem = df.premium.value_counts(dropna=False)
-print(c.head())
-print()
-print(prem.head())
+# print(c.head())
+# print()
+# print(prem.head())
+df = pd.get_dummies(df)
 
-# print(df.head())
+print(df.head())
